b3604ab4d661179bdaebf69a822c100f1eb436cd
Gino Zhu <gino.zhu@ziyi.io>
2023-02-15 09:58:16 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,80 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="left">
+      left
+    </div>
+    <div class="right">
+      <div ref="container" class="container">
+        <el-button class="icon-one" @click="onClickOne">Icon</el-button>
+        <i class="el-icon-full-screen full-screen-icon" @click="onFullScreen"/>
+        <dialog-one ref="DialogOne" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import DialogOne from './dialog-one.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    DialogOne
+  },
+  data () {
+    return {
+      isFullScreen: false
+    }
+  },
+  methods: {
+    onFullScreen () {
+      this.isFullScreen = !this.isFullScreen
+
+      if (this.isFullScreen) {
+        this.$refs.container.requestFullscreen()
+      } else {
+        document.exitFullscreen()
+      }
+    },
+    onClickOne () {
+      this.$refs.DialogOne.show()
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.home {
+  display: flex;
+  height: 100vh;
+}
+
+.left {
+  flex: 1;
+  background: #ddd;
+}
+
+.right {
+  flex: 3;
+}
+
+.container {
+  position: relative;
+  background: #777;
+  height: 50%;
+  width: 50%;
+}
+
+.icon-one {
+  position: absolute;
+  right: 20px;
+  top: 20px;
+}
+
+.full-screen-icon {
+  cursor: pointer;
+  position: absolute;
+  right: 20px;
+  bottom: 20px;
+}
+</style>
---
+++ src/views/dialog-one.vue
@@ -0,0 +1,79 @@
+<template>
+    <el-dialog
+        title="提示"
+        :visible.sync="dialogVisible"
+        width="30%">
+        <el-form :model="form">
+            <el-form-item label="item">
+                <el-select v-model="form.select" :popper-append-to-body="false" placeholder="请选择">
+                    <el-option
+                    v-for="item in options"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value">
+                    </el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item label="item">
+                <el-date-picker
+                    v-model="form.date"
+                    :append-to-body="false"
+                    type="daterange"
+                    range-separator="至"
+                    start-placeholder="开始日期"
+                    end-placeholder="结束日期">
+                </el-date-picker>
+            </el-form-item>
+        </el-form>
+        <span slot="footer" class="dialog-footer">
+            <el-button @click="dialogVisible = false">取 消</el-button>
+            <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+        </span>
+    </el-dialog>
+</template>
+
+<script>
+export default {
+  name: 'DialogOne',
+  props: {
+    fullscreen: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+      dialogVisible: false,
+      form: {
+        select: null,
+        date: null
+      },
+      options: [{
+        value: '选项1',
+        label: '黄金糕'
+      }, {
+        value: '选项2',
+        label: '双皮奶'
+      }, {
+        value: '选项3',
+        label: '蚵仔煎'
+      }, {
+        value: '选项4',
+        label: '龙须面'
+      }, {
+        value: '选项5',
+        label: '北京烤鸭'
+      }]
+    }
+  },
+  methods: {
+    show () {
+      this.dialogVisible = true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
